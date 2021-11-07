26c073a13fc5352eb5abfe3ae9c9a3369065bc8c
ClarkUPS <cbrace@pugetsound.edu>
2021-11-06 18:15:10 -0700

    made big progress again and set up the game detection system to make sure the player picks the correct correct color. Added temp prompt command for the colors till i figure out how to use user inputs.

--- simone.js
+++ simone.js
@@ -35,46 +35,79 @@ playButton.addEventListener("click", function () {
 
 async function playGame(colorList){   //play game and input array of colors to play 
     //main game for loop
+
+    await showPattern() //plays the welcome pattern
+
+    await timeout(4000) //4 second timeout before the start of the game
+
     for (let a = 0; a < colorList.length; a++) {
         alert("Round: " + (a+1));
-        await playPreview(colorList.slice(0, a+1)); //need to add so the slice works
+        await showPattern(colorList.slice(0, a+1), 120, 400); //need to add so the slice works
+        let pass = await testMemory(colorList.slice(0, a + 1));
+
+        if (pass == false) {
+           console.log("you fail");
+            break;
+        }
         //testMemory(colorList.slice(0, a));
 
         
     }
 
 }
-/*Function that gives the preview of what the player needs to do*/
-async function playPreview(subList) { 
+/*Function that displays a pattern given inputs*/
+async function showPattern(subList, blinkTime, spaceingTime) { 
     
     let boxSelect = document.querySelectorAll("div"); //should I reuse the select from earlier?
     
     for (let a = 0; a < subList.length; a++) {
         if (subList[a] == "red") {
             boxSelect[0].classList.add("lightred");
-            await timeout(120);
+            await timeout(blinkTime);
             boxSelect[0].classList.remove("lightred")
         }else if (subList[a] == "blue") {
             boxSelect[1].classList.add("lightblue");
-            await timeout(120);
+            await timeout(blinkTime);
             boxSelect[1].classList.remove("lightblue")
         } else if (subList[a] == "green") {
             boxSelect[2].classList.add("lightgreen");
-            await timeout(120);
+            await timeout(blinkTime);
             boxSelect[2].classList.remove("lightgreen")
         } else {
             boxSelect[3].classList.add("lightyellow");
-            await timeout(120);
+            await timeout(blinkTime);
             boxSelect[3].classList.remove("lightyellow")
         }
-        await timeout(400); //delay between rounds
+        await timeout(spaceingTime); //delay between rounds
     }
 }
 
 /*Function which tests the player on the correct color order sublist*/
 
 async function testMemory(sublist) {
+    let correctGuess = true;
     
+    for (let a = 0; a < sublist.length; a++) {
+        console.log("number of elements " + sublist.length);
+        userGuess = await guess();
+
+        if (userGuess != sublist[a]){
+            correctGuess = false;
+            break;
+        }
+
+
+
+    }
+    return correctGuess;
+}
+
+
+async function guess() {
+    
+    let guess = prompt("guess a color");
+    return guess;
+
 }
 
 
